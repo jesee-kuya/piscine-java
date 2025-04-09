0251851aa867d67932bcfe9fb84fc3d28e8ed47c
jkuya <kuyajesee@proton.me>
2025-04-10 01:33:52 +0300

    Add SortArgs class to sort and print command-line arguments

---
+++ SortArgs/SortArgs.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortArgs {
+    public static void sort(String[] args) {
+        Arrays.sort(args);
+        for (int i = 0 ; i < args.length; i++) {
+            if (i != args.length - 1) {
+             System.out.print(args[i]);
+             System.out.print(" ");
+            }else {
+                System.out.println(args[i]);
+            }
+        }
+    }
+}
