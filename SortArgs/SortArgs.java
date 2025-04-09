f1f2145982805b014682e1b1b075fec5f42894fc
jkuya <kuyajesee@proton.me>
2025-04-10 01:36:18 +0300

    Add check for empty argument array in SortArgs class

--- SortArgs/SortArgs.java
+++ SortArgs/SortArgs.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 public class SortArgs {
     public static void sort(String[] args) {
         Arrays.sort(args);
+        if (args.length == 0) {
+            System.out.println();
+            return;
+        }
+
         for (int i = 0 ; i < args.length; i++) {
             if (i != args.length - 1) {
              System.out.print(args[i]);
