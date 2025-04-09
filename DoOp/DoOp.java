fd880e06f09b31840a14d3ae041a0643d91d8363
jkuya <kuyajesee@proton.me>
2025-04-10 01:18:51 +0300

    Update input argument check in DoOp class to require exactly three arguments

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,6 +1,6 @@
 public class DoOp {
     public static String operate(String[] args) {
-        if (args.length < 3) {
+        if (args.length != 3) {
             return "Error";
         }
         int num1 = Integer.parseInt(args[0]);
